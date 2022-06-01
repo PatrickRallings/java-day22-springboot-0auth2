31b41a9efd3896d161d17fd408678f039e85a911
Patrick Rallings <Patrick.Rallings@gmail.com>
2022-06-01 14:47:54 -0400

    Initial commit

---
+++ src/main/java/com/example/javaday22springboot0auth2/JavaDay22Springboot0auth2Application.java
@@ -0,0 +1,13 @@
+package com.example.javaday22springboot0auth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDay22Springboot0auth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDay22Springboot0auth2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaday22springboot0auth2/JavaDay22Springboot0auth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaday22springboot0auth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDay22Springboot0auth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
